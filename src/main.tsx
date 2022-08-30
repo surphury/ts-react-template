648c74e15125ef932b33c8cb9ab1637777f29ee6
José Berríos <surphury@gmail.com>
2022-08-30 16:08:52 -0400

    updates dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { App } from './App';
 import 'normalize.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,17 @@ import react from '@vitejs/plugin-react';
 import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
 import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 
-import * as path from 'path';
+import * as path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })
 	],
-
+	/* 
 	define: {
 		'process.env': {}
-	},
+	}, */
 
 	resolve: {
 		alias: [
